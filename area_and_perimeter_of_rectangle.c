e8d3c435a98c39b69156d3b4ebd2369fda13e323
Sadik Hasin Baidya <sadik22319@gmail.com>
2026-08-20 20:58:38 +0530

    rectangle

--- area_and_perimeter_of_rectangle.c
+++ area_and_perimeter_of_rectangle.c
@@ -1,13 +1,14 @@
-#include<stdio.h>
-int main(){
-float a,area,b,p;
-printf("Enter the Length : ");
-scanf("%f", &a);
-printf("Enter the Base : ");
-scanf("%f", &b);
-p = 2*(a+b);
-area = (a*b);
-printf("The Perimeter Of Rectangle is : %f\n", p);
-printf("The Area Of Rectangle is : %f\n", area);
-return 0;
+#include <stdio.h>
+int main()
+{
+    float a, area, b, p;
+    printf("Enter the Length : ");
+    scanf("%f", &a);
+    printf("Enter the Base : ");
+    scanf("%f", &b);
+    p = 2 * (a + b);
+    area = (a * b);
+    printf("The Perimeter Of Rectangle is : %f\n", p);
+    printf("The Area Of Rectangle is : %f\n", area);
+    return 0;
 }
