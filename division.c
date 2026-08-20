ac9cb7ba0ace6c05a389af364ff634a3e1e3a766
Sadik Hasin Baidya <sadik22319@gmail.com>
2026-08-20 23:08:52 +0530

    divison of two number

---
+++ division.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    float a, b, c;
+    printf("Enter your first number (which you want to divide): \n");
+    scanf("%d", &a);
+    printf("Enter your second number(divisor): \n");
+    scanf("%d", &b);
+    c = a / b;
+    printf("The Final Answer is = %.2f", c);
+    return 0;
+}
