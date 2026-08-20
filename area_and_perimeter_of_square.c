011846a8d629b5e49a92186daa83b10922499e10
Sadik Hasin Baidya <142001486+sadik223119@users.noreply.github.com>
2026-08-20 19:42:14 +0530

    program to calculate area and perimeter of square

---
+++ area_and_perimeter_of_square.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+float a, area, p;
+printf("Enter the Side of Square : ");
+scanf("%f", &a);
+p = (4 * a);
+area = (a * a);
+printf("The Perimeter Of Square is : %f\n", p);
+printf("The Area Of Square is : %f\n", area);
+return 0;
+}
