ef1c1184c70d71912588add1bfcd7230b124b28b
Sadik Hasin Baidya <142001486+sadik223119@users.noreply.github.com>
2026-08-20 18:54:27 +0530

    program to subtract two integers

---
+++ subtraction_of_two_integer.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+  int a,b,c;
+  printf("Enter the First No : \n");
+  scanf("%d",&a);
+  printf("Enter the Second No : \n");
+  scanf("%d",&b);
+  c=(a-b);
+  printf("The Subtraction Of Two No is : %d \n",c);
+return 0;
+}
