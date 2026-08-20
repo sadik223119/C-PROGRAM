ea069983d9513d87a7a3f8d1902ecf6a59ab43cc
Sadik Hasin Baidya <sadik22319@gmail.com>
2026-08-20 20:32:28 +0530

    rectangle area and perimeter calculation

---
+++ area_and_perimeter_of_rectangle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+float a,area,b,p;
+printf("Enter the Length : ");
+scanf("%f", &a);
+printf("Enter the Base : ");
+scanf("%f", &b);
+p = 2*(a+b);
+area = (a*b);
+printf("The Perimeter Of Rectangle is : %f\n", p);
+printf("The Area Of Rectangle is : %f\n", area);
+return 0;
+}
