b348305e60d11b7c16dc20760555888a031bb7a2
Sadik Hasin Baidya <sadik22319@gmail.com>
2026-08-23 16:33:15 +0530

    finding nth element of fibonacci series by recursion

---
+++ n_th_elemet_of_fibonacci_series.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int fibonacci(int n)
+{
+    if (n == 1)
+
+        return 0;
+
+    else if (n == 2)
+        return 1;
+    else
+    {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter the position: ");
+    scanf("%d", &n);
+    printf("Fibonacci term (%d)= %d\n", n, fibonacci(n));
+
+    return 0;
+}
